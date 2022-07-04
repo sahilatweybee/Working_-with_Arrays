1d6eab454b38e3a2cdbde1d4ffa90d35a489106e
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-07-04 18:43:59 +0530

    Fixed the problerm of content not showing when loading the app

--- Bankist_app.js
+++ Bankist_app.js
@@ -65,7 +65,7 @@ const displayMovements = function (movements) {
     containerMovements.innerHTML = '';
     
     movements.forEach(function (mov, i) {
-        const type = mov > 0 ? 'diposit' : 'withdrawal';
+        const type = mov > 0 ? 'deposit' : 'withdrawal';
         const html = `
         <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
